f7c0753feb32616bd46a180b4281a219b7660c19
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-08 19:21:25 +0330

    main.lua implementing all prior changes to getter.lua defaults.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 local Defaults = require("defaults")
+local Getter = require(Defaults.Path.Getter)
 function love.update(dt)
-  
+  io.write(Defaults.Prompt.Input)
+  local Choice = io.read():upper()
+  if Choice == "EXIT" then love.event.quit()
+  else end
 end
