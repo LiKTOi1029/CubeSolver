19480817225676911a485be1dee35973bab69ddc
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-08 20:42:33 +0330

    data/cubes/regular/3x3.lua adding 3x3 cube template

--- data/cubes/regular/3x3.lua
+++ data/cubes/regular/3x3.lua
@@ -1,1 +1,9 @@
-
+local Template = {"W", "B", "O", "G", "R", "Y"}
+local X33 = {}
+for Index, Color in ipairs(Template) do
+  for SubIndex = 1, 9, 1 do
+	if SubIndex == 5 then X33[#X33+1] = "+"
+	else X33[#X33+1] = Color end
+  end
+end
+return X33
