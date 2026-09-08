0fe1e340f874eb599ed598fce877704ed73f9eb9
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-08 19:11:50 +0330

    getter.lua adding getter for commandfiles

--- getter.lua
+++ getter.lua
@@ -1,1 +1,13 @@
-
+local Getter = {}
+Getter.Commands = {}
+local Defaults = require("defaults")
+function Getter.Init()
+  Getter.RawCommands = love.filesystem.getDirectoryItems(Defaults.CommandsFolder)
+  for Index, CommandFile in ipairs(AllCommandFiles) do
+	local GSub = CommandFile:gsub("%.%a+$")
+	Getter.Commands[GSub:upper()] = require(Defaults.CommandsFolder .. GSub)
+	print(CommandFile, Index, GSub)
+  end
+end
+Getter.Init()
+return Getter
