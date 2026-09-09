f06fd3a84978f820604cae24e7ec8549ad9eefe3
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 09:43:30 +0330

    data/commands/help.lua adding bare bones, empty function

---
+++ data/commands/help.lua
@@ -0,0 +1,8 @@
+local Help = {}
+local Defaults = require("defaults")
+local InputProcessor = require(Defaults.Path.Helper.InputProcessor)
+local Tablify = require(Defaults.Path.Tablify)
+function Help.Execute(Input)
+  -- Not implemented
+end
+return Help
