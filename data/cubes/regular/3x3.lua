ecd37f6aff4dd3023f3aad151b57c9ab8c54c1f1
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 13:56:01 +0330

    data/cubes/regular/3x3.lua adding cube type into table

--- data/cubes/regular/3x3.lua
+++ data/cubes/regular/3x3.lua
@@ -1,5 +1,5 @@
 local Template = {"W", "B", "O", "G", "R", "Y"}
-local X33 = {}
+local X33 = {"3X3"}
 for Index, Color in ipairs(Template) do
   for SubIndex = 1, 9, 1 do
 	if SubIndex == 5 then X33[#X33+1] = "+"
