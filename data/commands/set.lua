34b52dcc6897cd63d4d69a9903d280dd691425f0
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 19:41:10 +0330

    data/commands/set.lua adding functionality for altering Status.Type

--- data/commands/set.lua
+++ data/commands/set.lua
@@ -7,6 +7,7 @@ function Set.Execute(Input)
   if type(Input) ~= "table" then io.write(Defaults.Prompt.Error, "Malformed input, please type help for help\n") end
   if Defaults.Path.Raw.Cubes[Input[1]] then
 	Status.Cube = Defaults.Path.Raw.Cubes[Input[1]]
+	Status.Type = Status.Cube[1]
 	io.write(Defaults.Prompt.Output, "Cube set to ", Input[1], "\n")
   else
 	io.write(Defaults.Prompt.Error, "The type", Input[1], " is invalid\n")
