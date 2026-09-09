de097c7c721c7feace5b7ff96a5f69f9430a58d9
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 09:41:24 +0330

    data/commands/get.lua data/commands/set.lua implementing both, but data/commands/get.lua only partially finished

--- data/commands/get.lua
+++ data/commands/get.lua
@@ -1,5 +1,13 @@
 local Get = {}
+local Defaults = require("defaults")
+local InputProcessor = require(Defaults.Path.Helper.InputProcessor)
+local Status = require(Defaults.Path.Status)
 function Get.Execute(Input)
-
+  Input = InputProcessor(Input)
+  if #Input == 0 then
+	io.write(Defaults.Prompt.Output, table.concat(Status.Cube), "\n")
+  else
+	-- This is not an error, it displays the saved cubes if a filename is inputted
+  end
 end
 return Get
--- data/commands/set.lua
+++ data/commands/set.lua
@@ -1,9 +1,12 @@
 local Defaults = require("defaults")
 local Status = require(Defaults.Path.Status)
+local InputProcessor = require(Defaults.Path.Helper.InputProcessor)
 local Set = {}
 function Set.Execute(Input)
-  if Defaults.Raw.Cubes[Input[1]] then
-	Status.Cube = Defaults.Raw.Cubes[Input[1]]
+  Input = InputProcessor(Input)
+  if type(Input) ~= "table" then io.write(Defaults.Prompt.Error, "Malformed input, please type help for help\n")
+  if Defaults.Path.Raw.Cubes[Input[1]] then
+	Status.Cube = Defaults.Path.Raw.Cubes[Input[1]]
   else
 	io.write(Defaults.Prompt.Error, "The type", Input[1], " is invalid\n")
   end
