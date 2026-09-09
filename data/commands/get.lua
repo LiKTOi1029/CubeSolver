d9994f23db0351f7177b668090427e132b9dc3b5
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 09:43:51 +0330

    data/commands/set.lua data/commands/get.lua bugfixing, now code works

--- data/commands/get.lua
+++ data/commands/get.lua
@@ -4,7 +4,7 @@ local InputProcessor = require(Defaults.Path.Helper.InputProcessor)
 local Status = require(Defaults.Path.Status)
 function Get.Execute(Input)
   Input = InputProcessor(Input)
-  if #Input == 0 then
+  if type(Input) == "number" then
 	io.write(Defaults.Prompt.Output, table.concat(Status.Cube), "\n")
   else
 	-- This is not an error, it displays the saved cubes if a filename is inputted
--- data/commands/set.lua
+++ data/commands/set.lua
@@ -4,9 +4,10 @@ local InputProcessor = require(Defaults.Path.Helper.InputProcessor)
 local Set = {}
 function Set.Execute(Input)
   Input = InputProcessor(Input)
-  if type(Input) ~= "table" then io.write(Defaults.Prompt.Error, "Malformed input, please type help for help\n")
+  if type(Input) ~= "table" then io.write(Defaults.Prompt.Error, "Malformed input, please type help for help\n") end
   if Defaults.Path.Raw.Cubes[Input[1]] then
 	Status.Cube = Defaults.Path.Raw.Cubes[Input[1]]
+	io.write(Defaults.Prompt.Output, "Cube set to ", Input[1], "\n")
   else
 	io.write(Defaults.Prompt.Error, "The type", Input[1], " is invalid\n")
   end
