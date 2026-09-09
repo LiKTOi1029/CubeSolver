58ecc52748880a4279d9c9c8905332765256f39b
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-09 20:00:32 +0330

    data/commands/move.lua adding new parameter to the moveset function call

--- data/commands/move.lua
+++ data/commands/move.lua
@@ -6,7 +6,7 @@ function Move.Execute(Input)
   Input = InputProcessor(Input)
   if type(Input) == "table" then
 	for Index, SingularMove in ipairs(Input) do
-	  Defaults.Path.Raw.Movesets[Status.Type].Execute(SingularMove)
+	  Defaults.Path.Raw.Movesets[Status.Type].Execute(SingularMove, Status.Cube)
 	end
   end
 end
