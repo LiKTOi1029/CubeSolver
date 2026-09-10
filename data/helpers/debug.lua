7eb9d21dc7751d0fadf6ef57092a838e6553d4d1
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-10 10:50:43 +0330

    data/helpers/debug.lua adding new debugging helper file

---
+++ data/helpers/debug.lua
@@ -0,0 +1,23 @@
+local Debug
+Debug.Number = 0
+Debug.File = ".log"
+Debug.Types = 
+{
+  ["INSERTION"] = function(Data)
+	local File = io.open(Debug.Number .. Debug.File, "r")
+	local FileData = File:read("*all")
+	if FileData:len() > 5000 then Debug.Number = Debug.Number + 1 end
+	File:close()
+	local File = io.open(Debug.Number .. Debug.File, "a")
+	File:write("\n", os.date(), "\n", Data, "\n")
+	File:close()
+  end
+}
+function Debug.Execute(Type, Data)
+  if Debug.Types[Type] then
+	Debug.Types[Type](Data)
+  else
+	io.write(Defaults.Prompt.Error)
+  end
+end
+return Debug
