efb769456f78cff1dd97f0c8c07f8b91f5017061
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-11 09:49:51 +0330

    data/movesets/regular/3x3.lua simplifying code and finalizing. BooleanTable[false][true](...) not implemented yet

--- data/movesets/regular/3x3.lua
+++ data/movesets/regular/3x3.lua
@@ -1,24 +1,29 @@
 local Defaults = require("defaults")
 local UniqueCopy = require(Defaults.Path.Helper.UniqueCopy)
-local BooleanTree = 
+local BooleanTree
+BooleanTree = 
 {
   [false] = 
   {
 	[true] = function(SubMoveset, Cube, Copy) -- DoubleMove
 	  
 	end,
-	[false] = function() -- Normal
-	  
+	[false] = function(SubMoveset, Cube, Copy) -- Normal
+	  for Index, FacePair in ipairs(SubMoveset) do
+		Cube[FacePair[1]] = Copy[FacePair[2]]
+	  end
 	end,
   },
   [true] = 
   {
-	[false] = function() -- Reversal
-	  
+	[false] = function(SubMoveset, Cube, Copy) -- Reversal
+	  for Index, FacePair in ipairs(SubMoveset) do
+		Cube[FacePair[2]] = Copy[FacePair[1]]
+	  end
 	end,
-	[true] = function() -- Reverse and DoubleMove
-	  
-	end,
+	[true] = function(...)
+	  io.write(Defaults.Prompt.Error, "Something went seriously wrong, BooleanTable was true, true\n")
+	end
   },
 }
 local Moveset = 
@@ -78,8 +83,8 @@ function Moveset.Execute(Input, Cube)
 	local SubMoveset = Moveset[Input]
 	local DoubleMove = Input:find("2")
 	local Reversal = Input:find("'")
-	if Reversal then BooleanTable[1] == true end
-	if DoubleMove then BooleanTable[2] == true end
+	if Reversal and not DoubleMove then BooleanTable[1] == true end
+	if DoubleMove and not Reversal then BooleanTable[2] == true end
 	Moveset.Mover(SubMoveset, Cube, BooleanTable)
   else
 	io.write(Defaults.Prompt.Error, Input, " is not a valid instruction\n")
