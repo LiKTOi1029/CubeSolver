e867edcb7fdcf626765adcaf7b7e8e653fb967d9
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-11 08:12:27 +0330

    data/commands/get.lua adding new algorithm for get command

--- data/commands/get.lua
+++ data/commands/get.lua
@@ -5,7 +5,17 @@ local Status = require(Defaults.Path.Status)
 function Get.Execute(Input)
   Input = InputProcessor(Input)
   if type(Input) == "number" then
-	io.write(Defaults.Prompt.Output, table.concat(Status.Cube), "\n")
+	io.write(Defaults.Prompt.Output, Status.Type, "\n")
+	local ParsingString = ""
+	for Index, Face in ipairs(Status.Cube) do
+	  if Index % 9 == 0 then
+		ParsingString = ParsingString..Face
+		io.write(Defaults.Prompt.Output, ParsingString, "\n")
+		ParsingString = ""
+	  else
+	    ParsingString = ParsingString..Face
+	  end
+	end
   else
 	-- This is not an error, it displays the saved cubes if a filename is inputted
   end
