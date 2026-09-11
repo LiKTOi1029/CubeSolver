89eed656ec560142194da27e2a6c04799094b3bf
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-11 10:06:35 +0330

    data/cubes/regular/3x3.lua adding new Execute() function specifically for generating new cubes from bare bones scratch

--- data/cubes/regular/3x3.lua
+++ data/cubes/regular/3x3.lua
@@ -1,9 +1,12 @@
-local Template = {"W", "B", "O", "G", "R", "Y"}
-local X33 = {"3X3"}
-for Index, Color in ipairs(Template) do
-  for SubIndex = 1, 9, 1 do
-	if SubIndex == 5 then X33[#X33+1] = "+"
-	else X33[#X33+1] = Color end
+local Cube = {}
+function Cube.Execute()
+  local Template = {"W", "B", "O", "G", "R", "Y"}
+  local X33 = {"3X3"}
+  for Index, Color in ipairs(Template) do
+    for SubIndex = 1, 9, 1 do
+  	  if SubIndex == 5 then X33[#X33+1] = "+"
+  	  else X33[#X33+1] = Color end
+    end
+    return X33
   end
-end
-return X33
+return Cube
