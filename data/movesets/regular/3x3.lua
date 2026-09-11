18fda8aac09565841a19da932b69caa791c89b63
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-11 18:30:44 +0330

    data/movesets/regular/3x3.lua adding comment at the top of the code to remind myself how it felt.

--- data/movesets/regular/3x3.lua
+++ data/movesets/regular/3x3.lua
@@ -1,3 +1,7 @@
+-- Note for future me:
+-- The cube mover function here is literally incorrect. It moves the cuboids incorrectly. 
+-- I just didn't have the energy to fix it because refer to readme.md, entry 2026/9/11, where I described 
+-- the experience in full detail.
 local Defaults = require("defaults")
 local UniqueCopy = require(Defaults.Path.Helper.UniqueCopy)
 local BooleanTree
