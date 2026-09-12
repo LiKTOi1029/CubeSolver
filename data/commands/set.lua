5ac22c8975377a7f662d48d393351abc4e80e0fb
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-12 16:45:26 +0330

    data/commands/set.lua implementing changes to data/cubes/regular/3x3.lua

--- data/commands/set.lua
+++ data/commands/set.lua
@@ -10,11 +10,8 @@ function Set.Execute(Input, Cube, Type)
   if type(Input) == "number" then io.write(Defaults.Prompt.Error, "Malformed input, please type help for help\n") return end
   if Defaults.Path.Raw.Cubes[Input[1]] then
 	local NewCube = CubeList[Input[1]].Execute()
-	Type = NewCube[1]
-	table.remove(NewCube, 1)
-	Cube = NewCube
 	io.write(Defaults.Prompt.Output, "Cube set to ", Input[1], "\n")
-	return Cube, Type
+	return NewCube
   else
 	io.write(Defaults.Prompt.Error, "The type", Input[1], " is invalid\n")
   end
