684ff1f3d3bed57cff827a7ef7930a8b3b1579cb
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-12 11:18:17 +0330

    main.lua implementing frontend side of the code

--- main.lua
+++ main.lua
@@ -1,13 +1,16 @@
 local Defaults = require("defaults")
 local Getter = require(Defaults.Path.Getter)
 local Tablify = require(Defaults.Path.Tablify)
-Getter.Commands["SET"].Execute({"set", "3X3"})
+local Status = require(Defaults.Path.Status)
+local Cube, Type = Getter.Commands["SET"].Execute({"set", "3X3"}, Status.Cube, Status.Type)
 
 function love.update(dt)
   io.write(Defaults.Prompt.Input)
   local Choice = Tablify.Execute("NORMAL", io.read():upper(), " ")
   if Choice[1] == "EXIT" then love.event.quit()
   elseif Getter.Commands[Choice[1]] then
-	Getter.Commands[Choice[1]].Execute(Choice)
+	local IfCube, IfType = Getter.Commands[Choice[1]].Execute(Choice, Cube, Type)
+	if type(IfCube) == "table" then Cube = IfCube end
+	if type(IfType) == "string" then Type = IfType end
   end
 end
